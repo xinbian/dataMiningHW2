3cbd5ff90ccfe15c8a5a1ff2f562ef9dd3e0814b
Xin Bian <bianxin2006@gmail.com>
2017-09-28 00:26:42 -0400

    finish need further graph code

--- 3.13.py
+++ 3.13.py
@@ -32,18 +32,21 @@ locData['country']=195
 
 #sort the dict based on its value
 locHie=sorted(locData.iteritems(), key=lambda d:d[1], reverse=True)
-labels={}
-for i in range(len(locHie)):
-    G.node(locHie[i][0])
-    if i>0:
-        G.edge(locHie[i][0],locHie[i-1][0])
 
-
-G.render('img/nominaldata')
-filename = G.render(filename='img/nominaldata')
-
-
-
+# =============================================================================
+# for i in range(len(locHie)):
+#     G.node(locHie[i][0])
+#     if i>0:
+#         G.edge(locHie[i][0],locHie[i-1][0])
+#         
+# #save hierarchy in svg
+# G.render('img/nominaldata')
+# filename = G.render(filename='img/nominaldata')
+# 
+# 
+# 
+# G = gv.Digraph(format='svg')
+# =============================================================================
 
 #this is for  The automatic generation of a concept hierarchy for 
 #numeric data based on the equal-width partitioning rule.
@@ -70,8 +73,58 @@ for i in range(len(numData)):
     
 
 
+# =============================================================================
+# for i in range(0,len(numHie)):
+#     G.node(str(i))
+#     if i>0:
+#         G.edge(str(i),str(i-1))
+#         
+# #save hierarchy in svg
+# G.render('img/numericaldata')
+# filename = G.render(filename='img/numericaldata')
+# 
+# 
+# 
+# 
+# G = gv.Digraph(format='svg')
+# =============================================================================
+#this is for  The automatic generation of a concept hierarchy for 
+#numeric data based on the equal-frequency partitioning rule.
+
+#suppose data is in a list
+#generate some random numbers as an example
+numData=random.sample(xrange(1000), 100)
+#specify how many intervals/bins
+bins=5
+#caluate frequency of each bin
+freq=len(numData)/bins
+#put values to bins
+#store final concept hieracry in dictionary, key as bin #
+numHie={}
+#initilize dict
+for key in range(bins):
+    numHie[key]=[]
+    
+numData=sorted(numData)
+#put values to bins
+flag=0
+for i in numData:
+    key = flag/freq
+    numHie[key].append(i)
+    flag += 1
 
 
+# =============================================================================
+# 
+# for i in range(0,len(numHie)):
+#     G.node(str(i))
+#     if i>0:
+#         G.edge(str(i),str(i-1))
+#         
+# #save hierarchy in svg
+# G.render('img/numericaldata2')
+# filename = G.render(filename='img/numericaldata2')
+# =============================================================================
 
 
 
